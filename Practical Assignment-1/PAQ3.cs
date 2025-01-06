e6e55404035ff48ba963928cd13b2a72fc726c2c
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-06 16:55:58 +0530

    as

---
+++ Practical Assignment-1/PAQ3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practical_Assignment_1
+{
+    internal class PAQ3
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14, r = 0, area = 0;
+
+            Console.Write("Enter radius of Circle : ");
+            r = Double.Parse(Console.ReadLine());
+
+            area = pi * r * r;
+
+            Console.WriteLine("Area of Circle is : " + area);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Practical Assignment-1/PSQ2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practical_Assignment_1
+{
+    internal class PSQ2
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Enter number : ");
+            a = Int32.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Entered number is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is odd");
+            }
+            Console.Read();
+        }
+
+    }
+}
