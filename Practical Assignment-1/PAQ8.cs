53cf51fb7340a904149992ae684584df7681c2d3
Kishan-Ramani <kishanramani269@gmail.com>
2025-01-06 17:52:25 +0530

    final

---
+++ Practical Assignment-1/PAQ8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practical_Assignment_1
+{
+    internal class PAQ8
+    {
+        static void Main(string[] args)
+        {
+            int sub1, sub2, sub3, total, avg, percentage;
+
+            Console.Write("Enter subject 1 marks : ");
+            sub1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter subject 2 marks : ");
+            sub2 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter subject 3 marks : ");
+            sub3 = Int32.Parse(Console.ReadLine());
+
+            total = sub1 + sub2 + sub3;
+            avg = total / 3;
+            percentage = (100*total)/300;
+
+            Console.WriteLine("Total : " + total);
+            Console.WriteLine("Average : " + avg);
+            Console.WriteLine("Percentage : " + percentage + "%");
+        }
+    }
+}
---
+++ Practical Assignment-1/PAQ8_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Practical_Assignment_1
+{
+    internal class PAQ8_1
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Enter number : ");
+            a = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(a + " * " + i + " = " + (a * i));
+            }
+        }
+    }
+}
